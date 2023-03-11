5cb61912704d0deb6d8b61f12bd65e77be5da7dc
Gracek3 <gracjan.lito@gmail.com>
2023-03-11 14:10:18 +0100

    text update every second

--- src/main/java/com/litit/timeleft/HelloController.java
+++ src/main/java/com/litit/timeleft/HelloController.java
@@ -1,10 +1,19 @@
 package com.litit.timeleft;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -12,7 +21,13 @@ public class HelloController implements Initializable {
     private Label welcomeText;
 
     private void init() {
-        welcomeText.setText("xzczxc");
+        Timeline timeline = new Timeline(
+            new KeyFrame(
+                Duration.millis(1000),
+                event -> welcomeText.setText(LocalDateTime.now().toString())
+        ));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     @Override
